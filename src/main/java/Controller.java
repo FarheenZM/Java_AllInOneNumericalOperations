7e715050ad2089d10b60b30a0600cf27158e9272
Farheen <farheenzm@gmail.com>
2019-02-11 11:45:49 +0000

    Input form on the home page added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -16,5 +16,6 @@ public class Controller {
             model.put("template", "templates/home.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, velocityTemplateEngine);
+        
     }
 }
