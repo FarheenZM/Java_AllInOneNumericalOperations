ccb31afa7679fb3b9d109f0643bad4495592f789
Farheen <farheenzm@gmail.com>
2019-02-11 11:11:07 +0000

    Initial setup and first reverse operation added

---
+++ src/main/java/NumericalOperations.java
@@ -0,0 +1,12 @@
+public class NumericalOperations {
+
+    public static int reverse(int number){
+        int reverse = 0;
+        while(number > 0){
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        return reverse;
+    }
+}
