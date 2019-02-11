1e619bd7e609001b99b4a213c3766a5e345edf9a
Farheen <farheenzm@gmail.com>
2019-02-11 11:35:26 +0000

    get request for home route added with a layout vtl

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,20 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class Controller {
     public static void main(String[] args) {
-        System.out.println(NumericalOperations.reverse(12345));
+        staticFileLocation("/public");
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+        get("/", (request, response) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/home.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
     }
 }
