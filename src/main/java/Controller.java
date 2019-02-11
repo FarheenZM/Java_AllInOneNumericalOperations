c1d6ab9e999d4cb7452f6dcdd3e28ee67a7bd0a8
Farheen <farheenzm@gmail.com>
2019-02-11 12:13:47 +0000

    Post request for the reverse route added

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -4,6 +4,7 @@ import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class Controller {
@@ -16,6 +17,14 @@ public class Controller {
             model.put("template", "templates/home.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, velocityTemplateEngine);
-        
+
+        post("/reverse", (request, response) -> {
+            String input = request.queryParams("numInput");
+            int number = Integer.parseInt(input);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("reverse", NumericalOperations.reverse(number));
+            model.put("template", "templates/reverse.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
     }
 }
