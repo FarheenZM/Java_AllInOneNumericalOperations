b4b089232d18f4248392e7a10217992c764f9008
Farheen <farheenzm@gmail.com>
2019-03-01 14:32:57 +0000

    Refactoring done to make a single route for all numerical operations

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -18,13 +18,47 @@ public class Controller {
             return new ModelAndView(model, "templates/layout.vtl");
         }, velocityTemplateEngine);
 
-        post("/reverse", (request, response) -> {
+        post("/", (request, response) -> {
             String input = request.queryParams("numInput");
             int number = Integer.parseInt(input);
             HashMap<String, Object> model = new HashMap<>();
-            model.put("reverse", NumericalOperations.reverse(number));
-            model.put("template", "templates/reverse.vtl");
+
+            model.put("input", input);
+
+//            model.put("test", request.queryParamOrDefault("click", "default"));
+            model.put("test", request.queryParams("click"));
+
+            if(model.get("test").equals("RReverse")) {
+                model.put("reverse", NumericalOperations.reverse(number));
+            }else if(model.get("test").equals("PPalindrome")) {
+                model.put("palindrome", NumericalOperations.palindrome(number));
+            }
+
+//            if(request.queryParams("click").equals("RReverse")){
+//                model.put("reverse", NumericalOperations.reverse(number));
+//            }else if(request.queryParams("click").equals("PPalindrome")) {
+//                model.put("palindrome", NumericalOperations.palindrome(number));
+//            }
+            model.put("template", "templates/home.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, velocityTemplateEngine);
+
+//        post("/reverse", (request, response) -> {
+//            String input = request.queryParams("numInput");
+//            int number = Integer.parseInt(input);
+//            HashMap<String, Object> model = new HashMap<>();
+//            model.put("reverse", NumericalOperations.reverse(number));
+//            model.put("template", "templates/reverse.vtl");
+//            return new ModelAndView(model, "templates/layout.vtl");
+//        }, velocityTemplateEngine);
+//
+//        post("/palindrome", (request, response) -> {
+//            String input = request.queryParams("numInput");
+//            int number = Integer.parseInt(input);
+//            HashMap<String, Object> model = new HashMap<>();
+//            model.put("palindrome", NumericalOperations.palindrome(number));
+//            model.put("template", "templates/palindrome.vtl");
+//            return new ModelAndView(model, "templates/layout.vtl");
+//        }, velocityTemplateEngine);
     }
 }
--- src/main/java/NumericalOperations.java
+++ src/main/java/NumericalOperations.java
@@ -9,4 +9,19 @@ public class NumericalOperations {
         }
         return reverse;
     }
+
+    public static boolean palindrome(int number){
+        int temp = number;
+        int reverse = 0;
+        while(number > 0){
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        if(temp == reverse){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
